bbfbaab0058d7c351cfc4325cc8df5522193406a
Dane Summers <dsummersl@yahoo.com>
2019-02-09 20:58:59 -0500

    Reorganize tmux commands into TmuxSession.

--- scripts/intmux.py
+++ scripts/intmux.py
@@ -1,32 +1,11 @@
 import argparse
 import logging
-import os.path
 import posix
-import subprocess
 import sys
 
-from . import connections
+from . import tmux
 
 logger = logging.getLogger('intmux')
-
-
-def tmux(command):
-    """ Run a tmux command """
-    command = "tmux {}".format(command)
-    logger.debug('tmux "{}"'.format(command))
-    subprocess.check_call([command], shell=True)
-
-
-def new_session(session):
-    """ Create a new tmux session"""
-    sessions = connections.check_output_as_list('tmux list-sessions -F "#S"')
-    if session in sessions:
-        print("Session '{}' already exists!".format(session))
-        sys.exit(posix.EX_USAGE)
-
-    command = "tmux new-session -d -s '{}'".format(session)
-    logger.debug('new_session "{}"'.format(command))
-    subprocess.check_call([command], shell=True)
 
 
 def main():
@@ -35,21 +14,27 @@ def main():
     )
 
     parser.add_argument(
-        '--command', '-c', default="", help="Command to execute when connecting to a remote host")
+        '--log', '-l', default="WARN",
+        help="Log level (default: WARN)")
+    parser.add_argument(
+        '--command', '-c', default="",
+        help="Command to execute when connecting to a remote host")
     parser.add_argument(
         '--input', '-i', nargs='?', type=argparse.FileType('r'), default=None,
-        help="Read list of hosts from input file.")
+        help="Read list of hosts from input file when provided, otherwise from STDIN.")
     parser.add_argument(
-        '--log', '-l', default="WARN", help="Log level (default: WARN)")
+        '--script', '-s', default="",
+        help="Execute commands in local file remotely")
+
     parser.add_argument(
-        '--panes', '-p', default=6, help="Max panes per window (default: 6)")
+        '--tmux-panes', '-p', default=6, metavar="PANES",
+        help="Max tmux panes per window (default: 6)")
     parser.add_argument(
-        '--script', '-s', default="", help="Execute commands in local file remotely")
+        '--tmux-sync', '-S', action='store_true',
+        help="Run tmux's set-option synchronize-panes on each tmux window")
     parser.add_argument(
-        '--sync', '-S', action='store_true',
-        help="Run set-option synchronize-panes on each tmux window")
-    parser.add_argument(
-        '--tmux', '-t', default='intmux', help="tmux session name (default: intmux)")
+        '--tmux-session', '-t', default='intmux', metavar="SESSION",
+        help="tmux session name (default: intmux)")
 
     subparsers = parser.add_subparsers(help='sub-command help', dest='subcommand')
 
@@ -84,89 +69,5 @@ def main():
         print('You must supply a subcommand.')
         sys.exit(posix.EX_USAGE)
 
-    if args.subcommand == 'ssh':
-        connection_type = connections.SSHConnection
-    elif args.subcommand == 'docker':
-        connection_type = connections.DockerConnection
-    elif args.subcommand == 'compose':
-        connection_type = connections.DockerComposeConnection
-    else:
-        print('Unknown subcommand type!')
-        sys.exit(posix.EX_USAGE)
-
-    hosts = []
-    if args.input:
-        for line in args.input.readlines():
-            hosts.append(line[:-1])
-    else:
-        hosts = connection_type.hosts(args)
-
-    if len(hosts) == 0:
-        print("At least one host must be specified!\n")
-        sys.exit(posix.EX_USAGE)
-
-    new_session(args.tmux)
-
-    # turn on window activity notification:
-    tmux("set-window-option -t {} -g monitor-activity on".format(args.tmux))
-    tmux("set-option -t {} -g visual-activity on".format(args.tmux))
-
-    cnt = 0
-    wcnt = 0
-    first = 1
-    made_new_window = True
-    for host in hosts:
-        logger.debug('Host = {}'.format(host))
-        if cnt < args.panes or args.panes == 0:
-            if first == 0:
-                tmux("split-window -t {}".format("{}:{}".format(args.tmux, wcnt)))
-            first = 0
-            cnt = cnt + 1
-        else:
-            if made_new_window and args.sync:
-                tmux("set-option -t {}:{} synchronize-panes".format(args.tmux, wcnt))
-                made_new_window = False
-            made_new_window = True
-            cnt = cnt + 1
-            wcnt = wcnt + 1
-            cnt = 1
-            tmux("new-window -t {}".format(args.tmux))
-            tmux("rename-window -t {}:{} {}".format(args.tmux, wcnt, host))
-            tmux("set-window-option -t {}:{} allow-rename off".format(args.tmux, wcnt))
-
-        if args.script and not os.path.exists(args.script):
-            print("{} does not exist!".format(args.script))
-            sys.exit(posix.EX_USAGE)
-
-        if args.script:
-            tmux("send-keys -t {}:{} \"{}\" C-m".format(
-                args.tmux, wcnt, connection_type.copy(host, args)))
-
-        tmux("send-keys -t {}:{} \"{}\" C-m".format(
-            args.tmux, wcnt, connection_type.connect(host, args)))
-
-        if args.command:
-            tmux("send-keys -t {}:{} {} C-m".format(args.tmux, wcnt, args.command))
-
-        if args.script:
-            basename = os.path.basename(args.script)
-            tmux("send-keys -t {}:{} 'chmod u+x /tmp/{} && /tmp/{}' C-m".format(args.tmux, wcnt, basename, basename))
-
-        tmux("select-layout -t {}:{} tiled".format(args.tmux, wcnt))
-
-    if made_new_window and args.sync:
-        logger.debug('synchronizing last window')
-        tmux("set-option -t {}:{} synchronize-panes".format(args.tmux, wcnt))
-
-    # remove session 0 - which is not connected to anything
-    # TODO provide a hotkey to run in all sessions
-
-    # Detect if we are already in a session. If we are, just switch to the other
-    # session:
-    if 'TMUX' in os.environ:
-        # When quitting out of this session, just switch to some other client
-        # (since there appears to be one already)
-        tmux("set-option -g detach-on-destroy off")
-        tmux("switch-client -t {}:{}".format(args.tmux, wcnt))
-    else:
-        tmux("attach-session -t {}:{}".format(args.tmux, wcnt))
+    session = tmux.TmuxSession(args)
+    session.connect()
---
+++ scripts/tmux.py
@@ -0,0 +1,125 @@
+import logging
+import os.path
+import subprocess
+
+from . import connections
+
+logger = logging.getLogger('tmux')
+
+
+def tmux(command):
+    """ Run a tmux command """
+    command = "tmux {}".format(command)
+    logger.debug('tmux "{}"'.format(command))
+    subprocess.check_call([command], shell=True)
+
+
+def new_session(session):
+    """ Create a new tmux session"""
+    sessions = connections.check_output_as_list('tmux list-sessions -F "#S"')
+    if session in sessions:
+        print("Session '{}' already exists!".format(session))
+        sys.exit(posix.EX_USAGE)
+
+    command = "tmux new-session -d -s '{}'".format(session)
+    logger.debug('new_session "{}"'.format(command))
+    subprocess.check_call([command], shell=True)
+
+
+class TmuxSession(object):
+
+    def __init__(self, args):
+        self.args = args
+        self.session = args.tmux_session
+        self.panes = args.tmux_panes
+        self.sync = args.tmux_sync
+        self.command = args.command
+        self.script = args.script
+
+        if args.script and not os.path.exists(args.script):
+            print("{} does not exist!".format(args.script))
+            sys.exit(posix.EX_USAGE)
+
+        if args.subcommand == 'ssh':
+            self.connection_type = connections.SSHConnection
+        elif args.subcommand == 'docker':
+            self.connection_type = connections.DockerConnection
+        elif args.subcommand == 'compose':
+            self.connection_type = connections.DockerComposeConnection
+        else:
+            print('Unknown subcommand type!')
+            sys.exit(posix.EX_USAGE)
+
+        self.hosts = []
+        if args.input:
+            for line in args.input.readlines():
+                self.hosts.append(line[:-1])
+        else:
+            self.hosts = self.connection_type.hosts(args)
+
+        if len(self.hosts) == 0:
+            print("At least one host must be specified!\n")
+            sys.exit(posix.EX_USAGE)
+
+    def connect(self):
+        new_session(self.session)
+
+        # turn on window activity notification:
+        tmux("set-window-option -t {} -g monitor-activity on".format(self.session))
+        tmux("set-option -t {} -g visual-activity on".format(self.session))
+
+        cnt = 0
+        wcnt = 0
+        first = 1
+        made_new_window = True
+        for host in self.hosts:
+            logger.debug('Host = {}'.format(host))
+            if cnt < self.panes or self.panes == 0:
+                if first == 0:
+                    tmux("split-window -t {}".format("{}:{}".format(self.session, wcnt)))
+                first = 0
+                cnt = cnt + 1
+            else:
+                if made_new_window and self.sync:
+                    tmux("set-option -t {}:{} synchronize-panes".format(self.session, wcnt))
+                    made_new_window = False
+                made_new_window = True
+                cnt = cnt + 1
+                wcnt = wcnt + 1
+                cnt = 1
+                tmux("new-window -t {}".format(self.session))
+                tmux("rename-window -t {}:{} {}".format(self.session, wcnt, host))
+                tmux("set-window-option -t {}:{} allow-rename off".format(self.session, wcnt))
+
+            if self.script:
+                tmux("send-keys -t {}:{} \"{}\" C-m".format(
+                    self.session, wcnt, self.connection_type.copy(host, self.args)))
+
+            tmux("send-keys -t {}:{} \"{}\" C-m".format(
+                self.session, wcnt, self.connection_type.connect(host, self.args)))
+
+            if self.command:
+                tmux("send-keys -t {}:{} {} C-m".format(self.session, wcnt, self.command))
+
+            if self.script:
+                basename = os.path.basename(self.script)
+                tmux("send-keys -t {}:{} 'chmod u+x /tmp/{} && /tmp/{}' C-m".format(self.session, wcnt, basename, basename))
+
+            tmux("select-layout -t {}:{} tiled".format(self.session, wcnt))
+
+        if made_new_window and self.sync:
+            logger.debug('synchronizing last window')
+            tmux("set-option -t {}:{} synchronize-panes".format(self.session, wcnt))
+
+        # remove session 0 - which is not connected to anything
+        # TODO provide a hotkey to run in all sessions
+
+        # Detect if we are already in a session. If we are, just switch to the other
+        # session:
+        if 'TMUX' in os.environ:
+            # When quitting out of this session, just switch to some other client
+            # (since there appears to be one already)
+            tmux("set-option -g detach-on-destroy off")
+            tmux("switch-client -t {}:{}".format(self.session, wcnt))
+        else:
+            tmux("attach-session -t {}:{}".format(self.session, wcnt))
